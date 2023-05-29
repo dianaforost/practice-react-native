f9e00638b044106943f865bfcfc21180d974db9e
Diana Forostiana <dianaforost@gmail.com>
2023-05-29 10:28:32 +0200

    Done

--- AwesomeProject/Screens/CreatePostsScreen.js
+++ AwesomeProject/Screens/CreatePostsScreen.js
@@ -1,30 +1,114 @@
-import { View, StyleSheet, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, Keyboard, ScrollView } from "react-native";
+import { Dimensions,Image, View, StyleSheet, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, Keyboard, ScrollView } from "react-native";
 import { Entypo } from '@expo/vector-icons'; 
 import { EvilIcons } from '@expo/vector-icons'; 
-import { useState } from 'react';
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import PostsScreen from "./PostsScreen";
+import { useState, useEffect, useRef } from 'react';
+import { Camera } from "expo-camera";
+import * as MediaLibrary from "expo-media-library";
+import * as Location from "expo-location";
+import { useNavigation } from '@react-navigation/native';
+
 
 export default function CreatePostsScreen(){
     const [title, setTitle] = useState("");
+    const [loc, setLoc] = useState ("")
   const [location, setLocation] = useState("");
-//   const Main = createBottomTabNavigator();
-    const onChangeLocation = (text) =>{
-        setLocation(text);
+  // console.log(location);
+  const [hasPermission, setHasPermission] = useState(null);
+  const [cameraRef, setCameraRef] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [capturedImage, setCapturedImage] = useState(null); 
+  const navigation = useNavigation();
+  // console.log(capturedImage);
+  useEffect(() =>{
+    (async () =>{
+      const { status } = await Camera.requestPermissionsAsync();
+      await MediaLibrary.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  },[])
+  const onPublish = async () => {
+    const { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      console.log("Permission to access location was denied");
+    } else {
+      const { coords } = await Location.getCurrentPositionAsync({});
+      const location = {
+        latitude: coords.latitude,
+        longitude: coords.longitude,
+      };
+      setLocation(location);
+      // console.log(location);
+  
+      navigation.navigate('PostsScreen', {
+        screen: 'PostsScreen',
+        params: {
+          title: title,
+          location: location,
+          loc: loc,
+          photo: capturedImage
+        }
+      });
+    }
+  };
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+  //   const Main = createBottomTabNavigator();
+  const onChangeLocation = (text) =>{
+      setLoc(text);
     }
     const onChangeText = (text) =>{
-        setTitle(text);
+      setTitle(text);
     }
-    const isButtonDisabled = title === "" || location === "";
+    const isButtonDisabled = title === "" || loc === "";
     return (
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <View style={styles.container}>
-            <View style={styles.imageDownloadCont}>
-            <View style={styles.photoCont}>
-            <View style={styles.imageCont}><Entypo name="camera" size={24} color="#BDBDBD" /></View>
-            </View>
+        <View style={styles.cont}>
+      <Camera
+        style={styles.camera}
+        type={type}
+        ref={setCameraRef}
+        >
+        <View style={styles.photoView}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={async () => {
+              if (cameraRef) {
+                const { uri } = await cameraRef.takePictureAsync();
+                await MediaLibrary.createAssetAsync(uri);
+                setCapturedImage(uri);
+                // console.log(capturedImage);
+              }
+            }}
+            ><View style={styles.imageCont}><Entypo name="camera" size={24} color="#fff" /></View>
+          </TouchableOpacity>
+            {/* <View style={styles.imageCont}><Entypo name="camera" size={24} color="#BDBDBD" /></View> */}
+            {/* <View style={styles.takePhotoOut}>
+              <View style={styles.takePhotoInner}></View>
+            </View> */}
+          <TouchableOpacity
+            style={styles.flipContainer}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                ? Camera.Constants.Type.front
+                : Camera.Constants.Type.back
+                );
+              }}
+              >
+            <Text style={{ fontSize: 18, color: "white"}}>
+              {" "}
+              Flip{" "}
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+    </View>
             <Text style={styles.downloadText}>Загрузите фото</Text>
-            </View>
             <ScrollView>
             <View>
                 <TextInput
@@ -40,78 +124,68 @@ export default function CreatePostsScreen(){
           style={styles.inputs}
           placeholder="Местность..."
           onChangeText={onChangeLocation}
-        />
+          />
       </View>
-      <TouchableOpacity style={[styles.btn, isButtonDisabled && styles.disabledBtn]} disabled={isButtonDisabled}>
+      <TouchableOpacity onPress={onPublish} style={[styles.btn, isButtonDisabled && styles.disabledBtn]} disabled={isButtonDisabled}>
             <Text style={styles.btnText}>Опубликовать</Text>
           </TouchableOpacity>
           </ScrollView>
+          <Image source={{ uri: capturedImage }}></Image>
         </View>
         {/* <Main.Navigator>
             <Main.Screen name="ProfileScreen" component={PostsScreen}></Main.Screen>
-        </Main.Navigator> */}
+          </Main.Navigator> */}
         </TouchableWithoutFeedback>
     )
-}
-const styles = StyleSheet.create({
+  }
+  const styles = StyleSheet.create({
     container:{
-        flex:1,
-        display:"flex",
-        // alignItems:"center",
-        paddingLeft:16,
-        paddingRight:16,
-        backgroundColor: "#FFFFFF",
-    },
-    photoCont:{
-        width:"100%",
-        height:240,
-        backgroundColor:"#E8E8E8",
-        display:"flex",
-        alignItems:"center",
-        justifyContent:"center",
-        marginBottom: 8
-    },
-    imageCont:{
-        padding: 18,
-        // borderRadius:"50",
-        backgroundColor:"#FFFFFF",
-        borderRadius: 50,
-    },
-    imageDownloadCont:{
-        marginBottom: 32
+      flex:1,
+      display:"flex",
+      // alignItems:"center",
+      paddingLeft:16,
+      paddingRight:16,
+      backgroundColor: "#FFFFFF",
     },
     downloadText:{
-        textAlign:"left"
+      textAlign:"left"
+    },
+    imageCont:{
+      padding: 18,
+      // borderRadius:"50",
+      backgroundColor:"#FFFFFF30",
+      borderRadius: 50,
+      top:20
     },
     input:{
-        paddingTop: 16,
-        paddingBottom: 15,
-        paddingLeft: 16,
-        // backgroundColor: "#F6F6F6",
-        borderBottomColor:"#E8E8E8",
-        borderBottomWidth:1,
-        marginBottom: 16
+      paddingTop: 16,
+      paddingBottom: 15,
+      paddingLeft: 16,
+      // backgroundColor: "#F6F6F6",
+      borderBottomColor:"#E8E8E8",
+      borderBottomWidth:1,
+      marginBottom: 16
     },
     inputContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        borderBottomColor: "#E8E8E8",
-        borderBottomWidth: 1,
-      },
-      inputs: {
-        paddingTop: 16,
-        paddingBottom: 15,
-        paddingLeft: 16,
-        flex: 1,
-        // backgroundColor: "#F6F6F6",
-        // marginBottom: 32
-      },
-      btn: {
-        paddingTop: 16,
-        paddingBottom: 16,
-        paddingLeft: 15,
-        backgroundColor: "#FF6C00",
-        marginBottom: 30,
+      flexDirection: 'row',
+      alignItems: 'center',
+      borderBottomColor: "#E8E8E8",
+      borderBottomWidth: 1,
+    },
+    inputs: {
+      paddingTop: 16,
+      paddingBottom: 15,
+      paddingLeft: 16,
+      flex: 1,
+      // backgroundColor: "#F6F6F6",
+      // marginBottom: 32
+    },
+    btn: {
+      paddingTop: 16,
+      paddingBottom: 16,
+      paddingLeft: 15,
+      backgroundColor: "#FF6C00",
+      marginBottom: 30,
         borderRadius: 100,
         marginTop:32
       },
@@ -125,4 +199,99 @@ const styles = StyleSheet.create({
       disabledBtn: {
         backgroundColor: "#CCC", // Цвет кнопки при отключенном состоянии
       },
+      cont: { flex: 2 },
+      camera: { 
+        flex: 2, display:"flex",
+        alignItems:"center", 
+        justifyContent:"center",
+        flexDirection:"row"
+      },
+      photoView: {
+        flex: 1,
+        backgroundColor: "transparent",
+        justifyContent: "center",
+        alignItems:"center"
+      },
+      
+      flipContainer: {
+        flex: 0.1,
+        alignSelf: "flex-end",
+      },
+      
+      button: { 
+        alignSelf: "center",
+        paddingBottom:10,
+      },
+      
+      takePhotoOut: {
+        borderWidth: 2,
+        borderColor: "white",
+        height: 50,
+        width: 50,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 50,
+      },
+      
+      takePhotoInner: {
+        borderWidth: 2,
+        borderColor: "white",
+        height: 40,
+        width: 40,
+        backgroundColor: "white",
+        borderRadius: 50,
+      },
 })
+        // const onPublish = async () => {
+          //   navigation.navigate('PostsScreen', {
+            //     screen: 'PostsScreen',
+        //     params: {
+          //       title: title,
+          //       location: location
+          //     }
+          //   });
+          
+          //   const { status } = await Location.requestForegroundPermissionsAsync();
+          //   if (status !== "granted") {
+            //     console.log("Permission to access location was denied");
+        //   } else {
+          //     let location = await Location.getCurrentPositionAsync({});
+          //     const coords = {
+            //       latitude: location.coords.latitude,
+        //       longitude: location.coords.longitude,
+        //     };
+        //     setLocation(coords);
+        //     console.log(location);
+        //   }
+        // };
+
+
+
+        
+        // const onPublish = () =>{
+        //   (async () =>{
+        //     const { status } =  Location.requestForegroundPermissionsAsync();
+        //     if (status !== "granted") {
+        //       console.log("Permission to access location was denied");
+        //     }
+        //     let location = Location.getCurrentPositionAsync({});
+        //     const coords = {
+        //       latitude: location.coords.latitude,
+        //       longitude: location.coords.longitude,
+        //     };
+        //     setLocation(coords);
+        //     console.log(location);
+        //     console.log(coords);
+        //   })
+        //   ();
+        //   navigation.navigate('PostsScreen', {
+        //     screen: 'PostsScreen',
+        //     params: {
+        //       title: title,
+        //       location: location,
+        //       loc: loc,
+        //       photo: capturedImage
+        //     }
+        //   });
+        // }
---
+++ AwesomeProject/Screens/MapScreen.js
@@ -0,0 +1,81 @@
+import { View, Text, StyleSheet } from "react-native";
+import { useState } from 'react';
+import { useRoute, useNavigation } from '@react-navigation/native';
+import * as Location from "expo-location";
+import MapView, { Marker } from 'react-native-maps';
+import { AntDesign } from '@expo/vector-icons'; 
+
+export default function MapScreen(){
+    const navigation = useNavigation();
+    // const [showMap, setShowMap] = useState(false);
+    const route = useRoute();
+    // console.log(route.params);
+
+    return (<>
+    <View style={styles.container}>
+    <View style={styles.header}>
+        <AntDesign
+          name="arrowleft"
+          size={24}
+          color="black"
+          onPress={() => {
+            navigation.goBack()
+          }}
+        />
+        <Text style={styles.title}>MapScreen</Text>
+      </View>
+        <View style={styles.mapContainer}>
+          <MapView style={styles.map} region={{
+  latitude: route.params.params.location.latitude,
+  longitude: route.params.params.location.longitude,
+  latitudeDelta: 0.0922,
+  longitudeDelta: 0.0421,
+}}>
+            <Marker title="Here" 
+            coordinate={{ latitude: 37.4220936, longitude: -122.083922 }}  
+            description="Hello"/>
+          </MapView>
+        </View>
+      {/* )} */}
+    </View>
+    </>
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    // icon: {
+    //   width: 50,
+    //   height: 50,
+    // },
+    header: {
+        flexDirection: 'row',
+        top:70,
+        // alignItems: 'center',
+        // paddingHorizontal: 10,
+        // paddingTop: 10,
+        // flex:3
+      },
+      title: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginLeft: 20,
+      },
+    mapContainer: {
+    //   ...StyleSheet.absoluteFillObject,
+      height: 800,
+      width:"100%",
+      top: 90,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+    },
+    map: {
+      ...StyleSheet.absoluteFillObject,
+    },
+  });
